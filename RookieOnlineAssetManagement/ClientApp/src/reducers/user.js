b3bc5dfaee098726e4b4ebcda979db4b73c6baca
Thanh Dat <RK10@nguyendoanphongitlive.onmicrosoft.com>
2021-05-10 17:41:04 +0700

    Create reducers, actions, api for redux user

---
+++ RookieOnlineAssetManagement/ClientApp/src/reducers/user.js
@@ -0,0 +1,38 @@
+import * as userManage from "../contains/ManageUser";
+
+const initialState = {
+  userList: [],
+  userSelected: {},
+  userLogin:{}
+};
+
+export default (state = initialState, { type, payload }) => {
+  switch (type) {
+    case userManage.USER_LIST: {
+      state.userList = payload;
+      return { ...state };
+    }
+    case userManage.USER_SELECTED: {
+      state.userSelected = payload;
+      return { ...state };
+    }
+    case userManage.ADD_USER: {
+      state.userList = state.userList.push(payload);
+      return { ...state };
+    }
+    case userManage.UPDATE_USER: {
+      state.userList = payload;
+      return { ...state };
+    }
+    case userManage.DISABLE_USER: {
+      state.userList = state.userList.filter((x) => x.id != payload);
+      return { ...state };
+    }
+    case userManage.GET_USER_LOGIN:{
+      state.userLogin=payload;
+      return {...state};
+    }
+    default:
+      return state;
+  }
+};
