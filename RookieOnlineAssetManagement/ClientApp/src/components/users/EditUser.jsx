6a900d2dc4a871f743d7b5d4353d23897f490932
Thanh Dat <RK10@nguyendoanphongitlive.onmicrosoft.com>
2021-05-10 17:44:43 +0700

    Update edit user page for react

--- RookieOnlineAssetManagement/ClientApp/src/components/users/EditUser.jsx
+++ RookieOnlineAssetManagement/ClientApp/src/components/users/EditUser.jsx
@@ -3,13 +3,95 @@ import Header from "../fragments/Header";
 import LeftSesstion from "../fragments/LeftSession";
 import { useHistory,useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import * as userManage from "../../actions/user";
 import { Input, Button } from "reactstrap";
 import "../../css/user_css/create.css";
 import Select from "react-select";
 import DateTimePicker from 'react-datetime-picker';
 
 export default function UpdateUser() {
- 
+  const dispatch = useDispatch();
+  const history=useHistory();
+  let {id}=useParams();
+
+  useEffect(() => {
+    dispatch(userManage.get_user_byId(id));
+  }, []);
+
+  const getUserById = useSelector((state) => state.user.userSelected);
+
+  let userByid=getUserById;
+
+  const [stateDateOfBirth, setDateOfBirth] = useState(new Date());
+  const [stateJoinedDate, setJoinedDate] = useState(new Date());
+
+  useEffect(()=>{
+    if(userByid.dateOfBirth!=undefined)
+      setDateOfBirth(new Date(userByid.dateOfBirth))
+  },[userByid.dateOfBirth])
+
+  useEffect(()=>{
+    if(userByid.joinedDate!=undefined)
+      setJoinedDate(new Date(userByid.joinedDate))
+  },[userByid.joinedDate])
+
+  const [updateUser, setUpdateUser] = useState({
+    DateOfBirth: stateDateOfBirth,
+    JoinedDate: stateJoinedDate,
+    Gender: null,
+    Type: null,
+  });
+
+  useEffect(()=>{
+    setUpdateUser({...updateUser,Gender:userByid.gender,Type:userByid.type})
+  },[userByid])
+
+  const [btnDisable, setBtnDisable] = useState(true);
+
+  const onSelectType = (e) => {
+    setUpdateUser({ ...updateUser, Type: e.value });
+  };
+
+  useEffect(() => {
+    if (
+      updateUser.DateOfBirth !== null &&
+      updateUser.JoinedDate !== null &&
+      updateUser.Type !== null &&
+      updateUser.Gender!==null
+    ) {
+      setBtnDisable(false);
+    } else {
+      setBtnDisable(true);
+    }
+  }, [updateUser]);
+
+  const [err,setErr]=useState({Check18YearsOld:""})
+
+  const onUpdate = () => {
+    const checkDay=stateJoinedDate.getDate()-stateDateOfBirth.getDate();
+    const checkMonth=stateJoinedDate.getMonth()-stateDateOfBirth.getMonth();
+    const checkYear=stateJoinedDate.getFullYear()-stateDateOfBirth.getFullYear();
+    if(checkDay>=0&&checkMonth==0&&checkYear==18||checkYear>18||checkYear==18&&checkMonth>0)
+    { 
+      dispatch(userManage.update_user(id,updateUser));
+    }
+    else{
+      setErr({...err,Check18YearsOld:"Chưa đủ 18 tuổi !"})
+    }
+  };
+
+  const optionsType = [
+    { value: true, label: "Admin" },
+    { value: false, label: "Staff" },
+  ];
+
+  useEffect(()=>{
+    setUpdateUser({...updateUser,DateOfBirth:stateDateOfBirth!=null?stateDateOfBirth:null})
+  },[stateDateOfBirth])
+
+  useEffect(()=>{
+    setUpdateUser({...updateUser,JoinedDate:stateJoinedDate!=null?stateJoinedDate:null})
+  },[stateJoinedDate])
 
   return (
     <div>
@@ -34,7 +116,7 @@ export default function UpdateUser() {
                     type="text"
                     readOnly
                     name="FirstName"
-                    
+                    value={userByid.firstName}
                   ></Input>
                 </div>
               </div>
@@ -47,7 +129,7 @@ export default function UpdateUser() {
                   <Input
                     type="text"
                     name="LastName"
-                    
+                    value={userByid.lastName}
                     readOnly
                   ></Input>
                 </div>
@@ -58,7 +140,7 @@ export default function UpdateUser() {
                   <label>Date of Birth</label>
                 </div>
                 <div className="col-8">
-                  <DateTimePicker format="dd/MM/y" clearIcon maxDate={new Date()} className="dateTimeCreateUser"></DateTimePicker>
+                  <DateTimePicker onChange={setDateOfBirth} value={stateDateOfBirth} format="dd/MM/y" clearIcon maxDate={new Date()} className="dateTimeCreateUser"></DateTimePicker>
                 </div>
               </div>
 
@@ -69,10 +151,10 @@ export default function UpdateUser() {
                 <div className="col-8">
                   <div className="row">
                     <div className="col-4 radioBtnCreateUser">
-                      <Input type="radio" /> Male
+                      <Input type="radio" value={true} onClick={()=>setUpdateUser({...updateUser,Gender:true})} checked={updateUser.Gender===true} /> Male
                     </div>
                     <div className="col-8">
-                      <Input type="radio" value={false}   /> Female
+                      <Input type="radio" value={false} onClick={()=>setUpdateUser({...updateUser,Gender:false})} checked={updateUser.Gender===false} /> Female
                     </div>
                   </div>               
                 </div>
@@ -83,8 +165,8 @@ export default function UpdateUser() {
                   <label>Joined Date</label>
                 </div>
                 <div className="col-8">
-                  <DateTimePicker format="dd/MM/y" clearIcon  className="d-ateTimeCreateUser"></DateTimePicker>
-                  <label className="validateErr"></label>
+                  <DateTimePicker format="dd/MM/y" value={stateJoinedDate} onChange={setJoinedDate} clearIcon minDate={stateDateOfBirth} className="dateTimeCreateUser"></DateTimePicker>
+                  <label className="validateErr">{err.Check18YearsOld}</label>
                 </div>
               </div>
 
@@ -94,8 +176,9 @@ export default function UpdateUser() {
                 </div>
                 <div className="col-8">
                   <Select
-                  
-                    
+                    options={optionsType}
+                    onChange={onSelectType}
+                    placeholder={updateUser.Type===true?"Admin":"Staff"}
                   ></Select>
                 </div>
               </div>
@@ -104,12 +187,12 @@ export default function UpdateUser() {
             <div className="row">
               <div className="col-6"></div>
               <div className="col-3">
-                <Button  color="danger">
+                <Button onClick={onUpdate} disabled={btnDisable} color="danger">
                   Save
                 </Button>
               </div>
               <div className="col-3">
-                <Button >Cancel</Button>
+                <Button onClick={()=>history.push('/user')}>Cancel</Button>
               </div>
             </div>
           </div>
